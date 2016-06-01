792f06d7f07156eb879b295e5c99241d0c8c49f5
David Wang <DevilDavidWang@gmail.com>
2016-06-01 05:04:39 -0400

    add support for additional command line options

--- server-models/main.cc
+++ server-models/main.cc
@@ -4,6 +4,7 @@
 void classical_single(const Config& config);
 void classical_fork(const Config& config);
 void classical_thread(const Config& config);
+void classical_thread_pool(const Config& config);
 
 using namespace std;
 
@@ -14,6 +15,18 @@ Config parse(int argc, char *argv[]) {
     config.host = argv[2];
     config.port = atoi(argv[3]);
     config.file = argv[4];
+
+    for (int i = 5; i < argc; i++) {
+        string item = argv[i];
+
+        if (item.find("-ps") == 0) {
+            string value = item.substr(3);
+            config.pool_size = atoi(value.c_str());
+        } else {
+            std::cerr << "Unknown option " << item << std::endl;
+            exit(1);
+        }
+    }
 
     return config;
 }
@@ -27,7 +40,8 @@ int main(int argc, char *argv[]) {
         classical_fork(config);
      } else if (config.mode == "thread") {
         classical_thread(config);
-    } else {
+     } else if (config.mode == "pool") {
+        classical_thread_pool(config);    } else {
         std::cerr << "unknown mode " << config.mode << std::endl;
     }
 
